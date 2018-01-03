3b79e75bd0d22a0fb00d3e252b41735b6cd55a6c
tidus747 <ivan.traceur@gmail.com>
2018-01-03 14:07:32 +0000

    Añadimos el tutoriales sobre el titulo de ejes

---
+++ Titulo_grafico/G_titulo_grafico.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+'''
+Iván Rodríguez - 2017
+Código generado para el canal de YouTube Piensa 3D
+'''
+
+# Importamos los módulos necesarios
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Generamos los datos para el gráfico
+x = np.array(range(500))*0.1
+y = np.zeros(len(x))
+for i in range(len(x)):
+    y[i] = math.sin(x[i])
+
+# Creamos el gráfico
+plt.ion()
+plt.plot(x,y)
+plt.xlabel("Coordenada X")
+plt.ylabel("Coordenada Y")
+
+plt.title("Representacion Seno")
